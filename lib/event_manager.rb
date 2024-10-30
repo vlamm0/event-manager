b18be114359386dcb7028e7d4c0ecc0d5a20b386
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-10-30 09:31:20 -0400

    Loading a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,10 @@
-puts "Event Manager Initialized!"
+puts "Event Manager Initialized!\n\n"
+
+File.exist?("event_attendees.csv") ? lines = File.readlines('event_attendees.csv') : raise("Terminating program")
+
+lines.each_with_index do |line, index|
+  next if index == 0
+  col = line.split(",")
+  name = col[2]
+  puts name
+end
