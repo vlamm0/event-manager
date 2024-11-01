787f25a151187f8229db7407e1bfcb0e432d2001
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-11-01 09:43:51 -0400

    Make unique form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,19 +1,19 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 
 
 # if the zip code is exactly five digits, assume that it is ok
 # if the zip code is more than five digits, truncate it to the first five digits
 # if the zip code is less than five digits, add zeros to the front until it becomes five digits
-def clean_zipcode(zipcode)
+def clean_zipcode(zipcode)  
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 # uses google-api to retrieve unique legislators for given zipcode
-def legislators_by_zipcode(zipcode)
-  # api
-  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+def legislators_by_zipcode(zipcode, civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new)
+  # api access
   civic_info.key = File.read('secret.key').strip
 
   # gets representatives by zip
@@ -22,24 +22,36 @@ def legislators_by_zipcode(zipcode)
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislators_names = legislators.map(&:name)
-    legislators_names.join(", ")
+    ).officials 
 
-  # if invalid zip gives website
+  # if invalid zip, give website
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
+# saves thank you letters into output folder
+def save_thank_you_letter(id, personal_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, "w").puts(personal_letter) 
+end
+
+
+#  main code
 puts "Event Manager Initialized!\n\n"
 File.exist?("event_attendees.csv") ? contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol) : raise("Terminating program")
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
 
 contents.each do |row|
+  # name and legislators are used in template binfing
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  
-  puts "#{name} #{zipcode} #{legislators}"
+
+  # create personalized html letter and save into output
+  personal_letter = erb_template.result(binding)
+  save_thank_you_letter(id, personal_letter)
 end
